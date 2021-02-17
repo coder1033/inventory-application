ca2d67bfea6cf9cb7cfa65c458eabfa9ea319910
Manan Jain <manan09mj@gmail.com>
2021-02-17 15:14:43 +0530

    add mongoDB and mongoose connection

--- app.js
+++ app.js
@@ -4,10 +4,20 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const catalogRouter = require('./routes/catalog');
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
+// For env variable DB_URL
+require('dotenv').config();
 
 var app = express();
+
+//Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = process.env.DB_CONNECTION_STRING;
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,7 +30,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/catalog", catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
